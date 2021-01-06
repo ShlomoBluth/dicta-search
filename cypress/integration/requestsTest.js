182fa327a66dc197a2db8a151ffa9b16b3d489ae
Shlomo Bluth <lomibluth@gmail.com>
2021-01-06 08:44:26 +0200

    RequestsTest

--- cypress/integration/requestsTest.js
+++ cypress/integration/requestsTest.js
@@ -12,12 +12,39 @@ describe('RequestsTest',()=>{
    
 
   
-  it('Message after request failed with seconds delay of response when clicking the run butten'+
-  ' of search page',()=>{
+  it('Error message for response with a delay of 5 minutes when clicking the run butten'+
+  ' of search page in hebrew mode',()=>{
     cy.searchRequest({
       language:'Hebrew',
       message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-      delaySeconds: 60*4
+      delaySeconds: 60*5
+    })
+  })
+
+  it('Error message for response with a delay of 5 minutes when clicking the run butten'+
+  ' of search page in english mode',()=>{
+    cy.searchRequest({
+      language:'English',
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+      delaySeconds: 60*5
+    })
+  })
+
+  it('Error message for response with status code 500 when clicking the run butten of search page'+
+  ' in hebrew mode',()=>{
+    cy.searchRequest({
+      language:'Hebrew',
+      status:500,
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+    })
+  })
+
+  it('Error message for response with status code 500 when clicking the run butten of search page'+
+  ' in english mode',()=>{
+    cy.searchRequest({
+      language:'English',
+      status:500,
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
     })
   })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,14 +25,40 @@ Cypress.Commands.add('searchRequest',({language,status=200,message='',delaySecon
         body:'it worked!',
         statusCode: status
     },).as('books')
+    cy.setLanguageMode(language)
     cy.get('input[id="search_box"]').type('בראשית ברא')
     cy.get('button[id="mobile_search_button"]').click({force:true})
-    cy.get('[class*="loader"').should('exist')
+    //cy.get('[class*="loader"').should('exist')
     cy.wait('@lexemes',{responseTimeout:1000*delaySeconds})
     cy.wait('@related',{responseTimeout:1000*delaySeconds})
     cy.wait('@wordforms',{responseTimeout:1000*delaySeconds})
     cy.wait('@search',{responseTimeout:1000*delaySeconds})
-    cy.wait('@books',{responseTimeout:1000*delaySeconds}).then(()=>{
-      cy.get('[class*="loader"').should('not.exist')
+    cy.wait('@books',{responseTimeout:1000*delaySeconds})
+
+    cy.get('@search').its('response.statusCode').should('eq',status)
+
+    if(message.length>0){
+        cy.contains(message).should('exist')
+    }
+})
+
+Cypress.Commands.add('setLanguageMode',(language)=>{
+    cy.get('body').then(elem => {
+      let languageMode
+      if(language=='Hebrew'){
+        languageMode='he'
+      }else if(language=='English'){
+        languageMode=''
+      }
+      let classAttr = elem.attr("class").substring(0,2);
+      if(classAttr!=languageMode)
+      {
+        cy.get('a').contains(/^English$/||/^עברית$/).click();
+      }
+      if(languageMode=='he'){
+        cy.get('a').contains(/^English$/).should('exist')
+      } else{
+        cy.get('a').contains(/^עברית$/).should('exist')
+      }
     })
 })
