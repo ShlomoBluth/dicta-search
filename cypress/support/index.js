881c78b15c7782b4a9b4a200444bc49a1119f8a9
Shlomo Bluth <lomibluth@gmail.com>
2021-03-14 14:56:44 +0200

    add commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,7 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './meaningsAndSynonymsCommands.js'
-import 'https://github.com/ShlomoBluth/searchShared.git/wordFormCommands.js'
+import 'https://ShlomoBluth/searchShared.github.io/wordFormCommands.js'
 import './booksCommand.js'
 
 // Alternatively you can use CommonJS syntax:
