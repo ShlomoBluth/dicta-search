b167eb8456d2e637c128c69831e92b40eee2471f
Shlomo Bluth <lomibluth@gmail.com>
2021-05-16 10:24:35 +0300

    add commit

--- cypress/integration/DevelopmentVersionRequestsTests.js
+++ cypress/integration/DevelopmentVersionRequestsTests.js
@@ -2,7 +2,7 @@
 
 ////run tests on requests from search run some in hebrew mode and english mode
 
-let sizes = ['iphone-x',[1000, 660]]
+let sizes = [[1000, 660]]//'iphone-x',
 
 sizes.forEach((size) => {
 
