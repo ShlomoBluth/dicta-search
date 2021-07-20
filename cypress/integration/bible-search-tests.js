5e388cbe2befb9369369e97761463b38cb715969
Shlomo Bluth <lomibluth@gmail.com>
2021-07-20 15:09:35 +0300

    add commit

--- cypress/integration/bible-search-tests.js
+++ cypress/integration/bible-search-tests.js
@@ -29,7 +29,7 @@ sizes.forEach((size) => {
                 })
                 cy.viewport(size)
             }
-            cy.visit('https://search.dicta.org.il/')
+            cy.visit('/')
         })
     
 
